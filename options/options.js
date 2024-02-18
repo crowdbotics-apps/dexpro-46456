d503ea6c07f1d77aa059051189f6228c41dd93d8
Crowdbotics <team@crowdbotics.com>
2024-02-18 23:23:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dexpro-46456",
+  url: "https://dexpro-46456.botics.co",
+  api: "https://dexpro-46456.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
